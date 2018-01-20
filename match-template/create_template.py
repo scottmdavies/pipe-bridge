d1ad203d5cac517bfb5576e35f0e00b9175330c5
scottmdavies <35192944+scottmdavies@users.noreply.github.com>
2018-01-20 16:35:30 +0000

    Update create_template.py
    Updated and tested

--- match-template/create_template.py
+++ match-template/create_template.py
@@ -1,14 +1,21 @@
-#
-
 import argparse
 import cv2
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", required=True, help="drawing file containing symbols", type=str)
 parser.add_argument("-o", "--output", required=True, help="location for saving cropped symbols", type=str)
 args = parser.parse_args()
 
+
+if os.path.exists(args.input)== False:
+    print("[WARNING] Symbol drawing location: not found")
+    exit()
 print("[INFO] Symbol drawing location: {}".format(args.input))
+
+if os.path.exists(args.output)== False:
+    print("[WARNING] Cropped symbol location: not found")
+    exit()
 print("[INFO] Cropped symbol location: {}".format(args.output))
 
 symbol_name = None
@@ -19,11 +26,14 @@ while True:
     cv2.destroyAllWindows()
     symbol_crop = symbol_drawing[int(roi[1]):int(roi[1] + roi[3]), int(roi[0]):int(roi[0] + roi[2])]
     symbol_name = input("Symbol name (exit to end): ")
-    if symbol_name == 'exit':
-        print("[INFO] Exiting '{}'".format(__file__))
-        break
-    elif symbol_name != 'exit':
-        symbol_file = symbol_name + '.png'
+
+    if symbol_name != 'exit':
+        base_path = args.output
+        symbol_file = os.path.join(base_path, (symbol_name +".png"))
         cv2.imwrite(symbol_file, symbol_crop)
         print(symbol_crop.shape[0:2])
         print("[INFO] Saving '{}'".format(symbol_file))
+
+    elif symbol_name == 'exit':
+        print("[INFO] Exiting '{}'".format(__file__))
+        break
